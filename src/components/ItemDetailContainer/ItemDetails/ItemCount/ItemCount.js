282e2f5305c1af06ca6c0ed28ed1d6ea3a0de44b
RamProg <ramirodlp@gmail.com>
2021-02-13 15:11:18 -0300

    added stock functionality

--- src/components/ItemDetailContainer/ItemDetails/ItemCount/ItemCount.js
+++ src/components/ItemDetailContainer/ItemDetails/ItemCount/ItemCount.js
@@ -1,34 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
 export const ItemCount = ({ stock, initial, onAdd, onFinish }) => {
-    const [contador, setContador] = useState(initial)
-    const [lessflag, setLessflag] = useState(false)
-    const [moreflag, setMoreflag] = useState(false)
+  const [contador, setContador] = useState(initial);
+  const [lessflag, setLessflag] = useState(false);
+  const [moreflag, setMoreflag] = useState(false);
 
-    const sumar = () => {
-        contador < stock && setContador(contador + 1)
-        stock - contador === 1 && setMoreflag(true)
-        setLessflag(false)
+  useEffect(() => {
+    if (initial === 0) {
+      setLessflag(true);
+      setMoreflag(true);
     }
-    const restar = () => {
-        contador && setContador(contador - 1)
-        contador === 1 && setLessflag(true)
-        setMoreflag(false)
+  }, []);
+
+  const sumar = () => {
+    contador < stock && setContador(contador + 1);
+    stock - contador === 1 && setMoreflag(true);
+    setLessflag(false);
+  };
+  const restar = () => {
+    contador && setContador(contador - 1);
+    contador === 1 && setLessflag(true);
+    setMoreflag(false);
+  };
+  const onEachAdd = () => {
+    if (contador) {
+      onAdd(contador);
+      onFinish();
     }
-    const onEachAdd = () => {
-        if (contador) {
-            onAdd(contador)
-            onFinish()
-        }
-    }
-    return (
-        <React.Fragment>
-            <div><div>
-                <button disabled={lessflag} className="btn btn-info" onClick={restar}>-</button>
-                <div className="font-weight-bold ml-2 mr-2">{contador}</div>
-                <button disabled={moreflag} className="btn btn-info" onClick={sumar}>+</button>
-            </div>
-                <button className="btn btn-outline-info" onClick={onEachAdd}>Añadir</button>
-            </div>        </React.Fragment>
-    )
-}
+  };
+  return (
+    <React.Fragment>
+      <div>
+        <div>
+          <button disabled={lessflag} className="btn btn-info" onClick={restar}>
+            -
+          </button>
+          <div className="font-weight-bold ml-2 mr-2">{contador}</div>
+          <button disabled={moreflag} className="btn btn-info" onClick={sumar}>
+            +
+          </button>
+        </div>
+        <button className="btn btn-outline-info" onClick={onEachAdd}>
+          Añadir
+        </button>
+      </div>{" "}
+    </React.Fragment>
+  );
+};
--- src/components/ItemDetailContainer/ItemDetails/ItemDetails.js
+++ src/components/ItemDetailContainer/ItemDetails/ItemDetails.js
@@ -1,67 +1,75 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from "react";
 // import {useParams} from 'react-router-dom'
-import './ItemDetails.css';
-import { ItemCount } from './ItemCount/ItemCount'
-import { useHistory } from 'react-router-dom'
-import { CartContext } from '../../../context/cartContext'
+import "./ItemDetails.css";
+import { ItemCount } from "./ItemCount/ItemCount";
+import { useHistory } from "react-router-dom";
+import { CartContext } from "../../../context/cartContext";
 
 export const ItemDetails = ({ item }) => {
+  const [finishFlag, setFinishFlag] = useState(false);
+  const [addedQuantity, setAddedQuantity] = useState(0);
+  const { addItem } = useContext(CartContext);
+  const history = useHistory();
 
-    const [finishFlag, setFinishFlag] = useState(false)
-    const [addedQuantity, setAddedQuantity] = useState(0)
-    const { addItem } = useContext(CartContext)
-    const history = useHistory();
+  const showFinish = () => {
+    setFinishFlag(true);
+  };
 
-    const showFinish = () => {
-        setFinishFlag(true)
+  function addHandler(quantityToAdd) {
+    if (quantityToAdd) {
+      setAddedQuantity(quantityToAdd);
     }
-    function addHandler(quantityToAdd) {
-        if (quantityToAdd) {
-            setAddedQuantity(quantityToAdd)
-        }
-    }
-    function finish() {
-        console.log("finish")
-        console.log(item, addedQuantity)
-        addItem(item, addedQuantity)
-        history.push("/cart");
-    }
+  }
 
-    function undo() {
-        setAddedQuantity(0)
-        setFinishFlag(false)
-    }
-   
+  function finish() {
+    console.log("finish");
+    console.log(item, addedQuantity);
+    addItem(item, addedQuantity);
+    history.push("/cart");
+  }
 
+  function undo() {
+    setAddedQuantity(0);
+    setFinishFlag(false);
+  }
 
+  function getInitial(stock) {
+    let initial = 1;
+    if (parseInt(stock) <= 0) initial = 0
+    return initial;
+  }
 
+  // ¡¡¡¡¡¡¡¡¡¡¡ ARREGLAR EL STOCK !!!!!!!!!!!!!!!
 
-
-// ¡¡¡¡¡¡¡¡¡¡¡ ARREGLAR EL STOCK !!!!!!!!!!!!!!!
-
-
-
-
-
-    return (
-        <React.Fragment>
-            <div class="item-container">
-                <div class="inside-item">
-                    <h1>{item.title}</h1>
-                    <h2>{item.description}</h2>
-                    <h3 class="price">{item.price}</h3>
-                    <img id="product-image" src={item.pictureUrl} alt="pic" />
-                    <div>{!addedQuantity &&
-                        <ItemCount stock={5} initial={1} onAdd={addHandler} onFinish={showFinish} />
-                    }
-                        {finishFlag && <div> Seleccionaste {addedQuantity} items <br />
-                            <button onClick={undo}>Deshacer</button>
-                            <button onClick={finish}>Agregar al carrito</button>
-                        </div>
-                        }
-                    </div>
-                </div>
-            </div>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <div class="item-container">
+        <div class="inside-item">
+          <h1> {item.title} </h1> <h2> {item.description} </h2>{" "}
+          <h3 class="price"> {item.price} </h3>{" "}
+          <img id="product-image" src={item.pictureUrl} alt="pic" />
+          <div>
+            {" "}
+            {!addedQuantity && <div>
+              <ItemCount
+                stock={item.stock}
+                initial={getInitial(item.stock)}
+                onAdd={addHandler}
+                onFinish={showFinish}
+              /> {parseInt(item.stock) <= 0 && <p>Sorry, there is no more stock</p>}
+            </div>}{" "}
+            {finishFlag && (
+              <div>
+                {" "}
+                Seleccionaste {addedQuantity}
+                items <br />
+                <button onClick={undo}> Deshacer </button>{" "}
+                <button onClick={finish}> Agregar al carrito </button>{" "}
+              </div>
+            )}{" "}
+          </div>{" "}
+        </div>{" "}
+      </div>{" "}
+    </React.Fragment>
+  );
+};
